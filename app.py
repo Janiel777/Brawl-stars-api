3be2b130b84efb0f2e246e56c6a648582397f6ba
Janiel <janjannunez777@gmail.com>
2024-06-01 16:53:36 -0400

    comentando el api de brawl stars

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-@app.route('/player/<tag>')
-def player(tag):
-    player_info = get_player_info(tag)
-    if player_info:
-        return f"Player: {player_info['name']}, Trophies: {player_info['trophies']}"
-    else:
-        return "Player not found", 404
+# @app.route('/player/<tag>')
+# def player(tag):
+#     player_info = get_player_info(tag)
+#     if player_info:
+#         return f"Player: {player_info['name']}, Trophies: {player_info['trophies']}"
+#     else:
+#         return "Player not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
